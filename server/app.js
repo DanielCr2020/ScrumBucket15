f017f732bf31574992b8d34e9375980be1f7d834
UnrealOG <creepercraft137@gmail.com>
2023-10-11 01:51:27 -0400

    more cors changes
    Routing finally works!

--- server/app.js
+++ server/app.js
@@ -9,13 +9,13 @@ import connection from './config/mongoConnection.js'
 async function main() {
     const db = await connection.dbConnection();
 }
-app.use(cors({origin:"*"}))
+app.use(cors())
 
-app.use(function(req,res,next) {
-    res.header('Access-Control-Allow-Origin',"*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
+// app.use(function(req,res,next) {
+//     res.header('Access-Control-Allow-Origin',"*")
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// })
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
