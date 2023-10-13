e5b8c1b8a786344a3dbe6a06905954e5911e703f
UnrealOG <creepercraft137@gmail.com>
2023-10-12 22:58:59 -0400

    access control allow methods middleware

--- server/app.js
+++ server/app.js
@@ -11,11 +11,12 @@ async function main() {
 }
 app.use(cors())
 
-// app.use(function(req,res,next) {
-//     res.header('Access-Control-Allow-Origin',"*")
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// })
+app.use(function(req,res,next) {
+    res.header('Access-Control-Allow-Origin',"*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH")
+    next();
+})
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
