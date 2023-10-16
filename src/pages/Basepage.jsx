81f6ef76d1d4a44089a69d5050287bf8c127fc78
jmcerino <jmcerino13@gmail.com>
2023-10-16 14:12:13 -0400

    General UI changes along with buttons for sign up and sign in

--- src/pages/Basepage.jsx
+++ src/pages/Basepage.jsx
@@ -8,9 +8,9 @@ function Basepage(){
         <div>
             <h1>Welcome to the Skill Sharing Site!</h1>
             <h3>Connect with your local community by learning skills taught by those around you.</h3>
-            <A class={styles.link} href="/home">Go to your homepage</A> <br /> <br />
-            <A class={styles.link} href="/signup">Click here to register</A> <br /> <br />
-            <A class={styles.link} href="/login">Click here to log in</A>
+            <A class={styles.basepage_link} href="/home">Go to your homepage</A> <br /> <br />
+            <A class={styles.basepage_link} href="/signup">Click here to register</A> <br /> <br />
+            <A class={styles.basepage_link} href="/login">Click here to log in</A>
         </div>
     )
 }
