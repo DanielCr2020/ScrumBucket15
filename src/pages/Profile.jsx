2b1209c91916f305a36974a9163118aa5a2a533b
Annabelle Crescenzo <crescenzoa.01@gmail.com>
2023-11-15 12:37:28 -0500

    skills listed on profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -38,19 +38,25 @@ function Profile(props){
                 </div>
                 <div class={styles.userProfileInfoDiv}>
                     <div class={styles.skillInterestDiv}>
-                        <h3 class={styles.skillHeader}>My Known Skills:</h3>
+                        <h3 class={styles.skillHeader}>My Skills:</h3>
                         <div class={styles.profileListDiv}>
                             <list class={styles.profileList}>
                                 {/* patch request to api/users/profile/updateSkills */}
-                                <For each={profileInfo()["skills"]}>
-                                    {(item) => <div>{JSON.stringify(item)}</div>}
-                                </For>
+                                <ul>
+                                    <For each={profileInfo()["skills"]}>
+                                        {(item) => (
+                                            <li>
+                                                <div>  <strong> {item.skillName} </strong> -- Proficiency: {item.proficiency} </div>
+                                            </li>
+                                        )}
+                                    </For>
+                                </ul>
                                 <Show when={JSON.stringify(profileInfo()["skills"]) == "{}"}>
-                                    <li>No skills, add one!</li>
+                                    <li>No skills to share, add one!</li>
                                 </Show>
                                 <br></br>
                                 <Show when={!newSkill()}>
-                                    <button class={styles.newSkillButton} onClick={[handleClick]}>Click to add, update, or delete a skill!</button>
+                                    <button class={styles.newSkillButton} onClick={[handleClick]}>Click to Add, Update, or Delete a Skill!</button>
                                 </Show>
                                 <Show when={newSkill()}>
                                     <NewSkill 
@@ -62,18 +68,35 @@ function Profile(props){
                             </list>
                         </div>
                     </div>
-                    <Show when={profileInfo()["isMentor"]==true}>
-                        <div class={styles.skillTeachDiv}>
-                            <h3 class={styles.skillHeader}>My Skill Interests:</h3>
-                            <div class={styles.profileListDiv}>
-                                <list class={styles.profileList}>
-                                    <li>Pre calculus</li>
-                                    <li>Watercolor</li>
-                                    <li>Poetry</li>
-                                </list>
-                            </div>
+                    {/* Add new section for "Looking For:" skills */}
+                    <div class={styles.skillInterestDiv}>
+                        <h3 class={styles.skillHeader}>Looking For:</h3>
+                        <div class={styles.profileListDiv}>
+                            <list class={styles.profileList}>
+                                {/* patch request to api/users/profile/updateLookingForSkills (needs to be created)*/}
+                                <ul>
+                                    <For each={profileInfo()["lookingForSkills"]}>
+                                            {(item) => (
+                                                <li>
+                                                    <div>  <strong> {item.skillName} </strong> -- Proficiency: {item.proficiency} </div>
+                                                </li>
+                                            )}
+                                    </For>
+                                </ul>
+                                <Show when={JSON.stringify(profileInfo()["lookingForSkills"]) == "{}"}>
+                                    <li>No skills to learn, add one!</li>
+                                </Show>
+                                {/* button to add/delete skills user wants to learn */}
+{/*                                 <Show when={newSkill()}>
+                                    <NewSkill 
+                                        url={props.url}
+                                        element={props.element} 
+                                        setNewSkill={setNewSkill} 
+                                        updatedUser={JSON.stringify(profileInfo()["displayName"])}/>
+                                </Show> */}
+                            </list>
                         </div>
-                    </Show>
+                    </div>
                 </div>
             </Show>
         </div>
