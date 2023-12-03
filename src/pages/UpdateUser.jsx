f06117202ca234f2a3e04687ecf120e7063d85be
jmcerino <jmcerino13@gmail.com>
2023-12-03 15:09:34 -0500

    Updated user info update page

--- src/pages/UpdateUser.jsx
+++ src/pages/UpdateUser.jsx
@@ -5,8 +5,9 @@ import clientValidation from "../clientValidation";
 function UpdateUser(props) {
   // updatedUser, newSkill, newProficiency, userId
   const [userData, setUserData] = createSignal({
-    newEmail: "",
+    newContact: "",
     newDescription: "",
+    newDisplayname: "",
   });
   const [creatingUser, setCreatingUser] = createSignal(false); //used for hiding the signup button until the user is successfully created
   const [error, setError] = createSignal(null);
@@ -16,8 +17,9 @@ function UpdateUser(props) {
     console.log("skill data:", userData());
     try {
       //validate input on frontend. (If the data is bad, we can catch it before it goes to the server)
-      clientValidation.checkEmail(userData().newEmail);
+      clientValidation.checkContact(userData().newContact);
       clientValidation.checkDesc(userData().newDescription);
+      clientValidation.checkDisplayName(userData().newDisplayname);
     } catch (e) {
       setError(e);
       console.log("Client error:", e);
@@ -55,9 +57,14 @@ function UpdateUser(props) {
         <p class={styles.error}>{error()}</p>
       </Show>
       <form onSubmit={submitForm} id="user-update-form">
-        <label for="newEmail">New Email: </label>
-        <input id="newEmail" onChange={handleChange}>
-          Email
+        <label for="newDisplayname">New Display Name: </label>
+        <input id="newDisplayname" onChange={handleChange}>
+          DisplayName
+        </input>{" "}
+        <br />
+        <label for="newContact">New Contact: </label>
+        <input id="newContact" onChange={handleChange}>
+          Contact
         </input>{" "}
         <br />
         <label for="newDescription">New Description: </label>
